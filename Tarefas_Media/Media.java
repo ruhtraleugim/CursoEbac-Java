77b91bd842f1c5732783fd6b23cb1def8f76fb14
Arthur Miguel <136189421+ruhtraleugim@users.noreply.github.com>
2024-02-04 07:27:24 -0300

    adição da tarrefa do modulo 8

---
+++ Tarefas_Media/Media.java
@@ -0,0 +1,26 @@
+
+
+public class Media {
+
+
+
+    public static void main(String[] args) {
+        mediaDosAlunos();
+        
+    
+
+    }
+
+    private static void mediaDosAlunos() {
+        float aluno1 = 1;
+        float aluno2 = 2;
+        float aluno3 = 9;
+        float aluno4 = 8;
+        float MediaAlunos = (aluno1+aluno2+aluno3+aluno4)/4;
+        System.out.println(MediaAlunos);
+    }
+
+
+
+
+}

576465243096b57684a76730e5a6fb6168c44031
Arthur Miguel <136189421+ruhtraleugim@users.noreply.github.com>
2024-04-06 15:04:31 -0300

    Add files via upload

---
+++ tareda_moduloGenerics/SUV.java
@@ -0,0 +1,9 @@
+import java.sql.Date;
+
+public class SUV extends Car {
+
+    public SUV(String modelo, double preço, String cor, int ano) {
+        super(modelo, preço, cor, ano);
+    }
+    
+}
---
+++ tareda_moduloGenerics/Sedan.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Sedan extends Car{
+
+    public Sedan(String modelo, double preço, String cor, int ano) {
+        super(modelo, preço, cor, ano);
+    }
+}

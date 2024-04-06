307cd34806ba95645a191115635f63f68f22e69c
Arthur Miguel <136189421+ruhtraleugim@users.noreply.github.com>
2024-04-06 15:05:08 -0300

    Add files via upload

---
+++ tareda_moduloGenerics/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Car> cars = new ArrayList<>();
+
+        cars.add(new SUV("x6",1_000_000, "blue", 2023));
+
+    }
+}

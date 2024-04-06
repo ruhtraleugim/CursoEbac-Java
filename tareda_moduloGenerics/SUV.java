e27cad638e228769d8362d0296b2a6ba347d0e83
Arthur Miguel <136189421+ruhtraleugim@users.noreply.github.com>
2024-04-06 15:04:55 -0300

    Delete tareda_moduloGenerics directory

--- tareda_moduloGenerics/SUV.java
+++
@@ -1,9 +0,0 @@
-import java.sql.Date;
-
-public class SUV extends Car {
-
-    public SUV(String modelo, double preço, String cor, int ano) {
-        super(modelo, preço, cor, ano);
-    }
-    
-}
--- tareda_moduloGenerics/Sedan.java
+++
@@ -1,8 +0,0 @@
-import java.util.Date;
-
-public class Sedan extends Car{
-
-    public Sedan(String modelo, double preço, String cor, int ano) {
-        super(modelo, preço, cor, ano);
-    }
-}
